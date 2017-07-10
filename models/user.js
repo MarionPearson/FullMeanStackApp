736d6cf23bdab3ca2efc1fba3c644e0e34544ea5
Marion Pearson <marionpearson94@gmail.com>
2017-07-10 13:52:48 -0500

    Fleshed out the user registration. Used bcrypt to hash user passwords. Input can now be taken into the API through a form to be later implemented. Salt hash is verified.

---
+++ models/user.js
@@ -0,0 +1,51 @@
+//This file sets up a JSON schema using mongoose for each user, as well as their corresponding queries.
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+//Import the config file for the user database
+const config = require('../config/database');
+
+//Schema for the users
+const userSchema = mongoose.Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+
+});
+
+//Exports the user with paramaters for the user and schema related to it
+const User = module.exports = mongoose.model('User', userSchema);
+
+//Sets up queries to find users either by ID (getUserByID) or username (getUserByUserName)
+module.exports.getUserByID = function(id, callback){
+    user.findByID(id, callback);
+}
+
+module.exports.getUserByUsername = function(iusername, callback){
+    const query = {username: username};
+    user.findOne(username, callback);
+}
+
+//Generates a salt key using bcrpyt for information encryption
+module.exports.addUser = function(newUser, callback){
+    bcrypt.genSalt(10, (err,salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if(err) throw err; //:O
+            newUser.password = hash;
+            newUser.save(callback);
+        })
+    });
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+//Brings in the user schema for user information
+const User = require('../models/user');
 
 //Routing for user registration
-router.get('/register', (req, res, next) => {
-    res.send('REGISTERED');
+router.post('/register', (req, res, next) => {
+    //Grabs form information to be encrypted by bcrypt
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    })
+
+    User.addUser(newUser, (err,user) =>{
+        if(err){
+            res.json({success: false, msg:'Registration failed'});
+        }
+        else{
+            res.json({succes: true, msg:'User Registered'});
+        }
+    })
 });
 
 //Routing for authentication
@@ -16,9 +36,5 @@ router.get('/profile', (req, res, next) => {
     res.send('PROFILE');
 });
 
-//Routing for validation
-router.get('/validate', (req, res, next) => {
-    res.send('VALIDATED');
-});
 
 module.exports = router;
